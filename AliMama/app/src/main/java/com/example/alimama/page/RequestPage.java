1df338d16236615b5c879c6b901553b08ab9d200
King Fu <kingfu1012@gmail.com>
2019-11-01 15:30:20 -0600

    Update

--- AliMama/app/src/main/java/com/example/alimama/page/RequestPage.java
+++ AliMama/app/src/main/java/com/example/alimama/page/RequestPage.java
@@ -1,51 +1,51 @@
-package project.mood.page;
-
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-
-import project.mood.R;
-import project.mood.model.Contact;
-
-import project.mood.ui.main.RequestPageAdapter;
-
-public class RequestPage extends AppCompatActivity {
-    RecyclerView recyclerView;
-    RequestPageAdapter requestPageAdapter;
-    ArrayList<Contact> contactDataList;
-
-
-    public static final String[] contactList = {"ALPHA", "BETA", "GAMMA", "DELTA", "THETA", "OMEGA", "ZETA"};
-
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
-
-        /*Recycler view setup*/
-        ArrayList<Contact> contactDataList = new ArrayList<Contact>();
-        for (int i = 0; i < contactList.length; i++) {
-            Contact contact = new Contact();
-
-            contact.setContactName(contactList[i]);
-
-            contactDataList.add(contact);
-        }
-
-        requestPageAdapter = new RequestPageAdapter(contactDataList);
-
-        recyclerView = findViewById(R.id.my_recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(requestPageAdapter);
-
-
-    }
-}
+//package project.mood.page;
+//
+//import android.os.Bundle;
+//
+//import androidx.annotation.Nullable;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.recyclerview.widget.DefaultItemAnimator;
+//import androidx.recyclerview.widget.LinearLayoutManager;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import java.util.ArrayList;
+//
+//import project.mood.R;
+//import project.mood.model.Contact;
+//
+//import project.mood.ui.main.RequestPageAdapter;
+//
+//public class RequestPage extends AppCompatActivity {
+//    RecyclerView recyclerView;
+//    RequestPageAdapter requestPageAdapter;
+//    ArrayList<Contact> contactDataList;
+//
+//
+//    public static final String[] contactList = {"ALPHA", "BETA", "GAMMA", "DELTA", "THETA", "OMEGA", "ZETA"};
+//
+//
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main_activity);
+//
+//        /*Recycler view setup*/
+//        ArrayList<Contact> contactDataList = new ArrayList<Contact>();
+//        for (int i = 0; i < contactList.length; i++) {
+//            Contact contact = new Contact();
+//
+//            contact.setContactName(contactList[i]);
+//
+//            contactDataList.add(contact);
+//        }
+//
+//        requestPageAdapter = new RequestPageAdapter(contactDataList);
+//
+//        recyclerView = findViewById(R.id.my_recycler_view);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setItemAnimator(new DefaultItemAnimator());
+//        recyclerView.setAdapter(requestPageAdapter);
+//
+//
+//    }
+//}
