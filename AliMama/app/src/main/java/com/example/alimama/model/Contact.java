091a13e255101cb6e3f2c56be69fd24f31c0f534
SkyZhangX <51038567+SkyZhangX@users.noreply.github.com>
2019-11-01 17:05:01 -0600

    Add files via upload

---
+++ AliMama/app/src/main/java/com/example/alimama/model/Contact.java
@@ -0,0 +1,19 @@
+package com.example.alimama.model;
+
+public class Contact {
+
+    private String contactName;
+
+
+    public Contact(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactName() {
+        return this.contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+}
