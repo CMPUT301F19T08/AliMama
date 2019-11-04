6d44a9c85f2e9773a98ac3a6d7b1f1d0bf0f03c6
King Fu <kingfu1012@gmail.com>
2019-11-04 00:35:23 -0700

    Update Mood UI

--- AliMama/app/src/main/java/com/example/alimama/Mainmenu.java
+++ AliMama/app/src/main/java/com/example/alimama/Mainmenu.java
@@ -29,15 +29,6 @@ public class Mainmenu extends AppCompatActivity {
         viewMoodMapButton = findViewById(R.id.mood_map_button);
         viewOrAddFriendsButton = findViewById(R.id.add_view_friend_button);
         logoutButton = findViewById(R.id.logout_button);
-        Button button = findViewById(R.id.button2);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent editMood = new Intent(Mainmenu.this, EditMoodEvent.class);
-                startActivity(editMood);
-            }
-        });
 
         viewMoodHistoryButton.setOnClickListener(new View.OnClickListener() {
             @Override
