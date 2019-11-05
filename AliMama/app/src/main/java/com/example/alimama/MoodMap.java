ffeb9c49d597ae274a4d1077496e984cc2ee818f
XuechunHou <xhou1@ualberta.ca>
2019-11-05 01:23:03 -0700

    updated MoodMap, changed layout design.

--- AliMama/app/src/main/java/com/example/alimama/MoodMap.java
+++ AliMama/app/src/main/java/com/example/alimama/MoodMap.java
@@ -1,55 +1,72 @@
 package com.example.alimama;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
+import android.content.Context;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import androidx.core.app.ActivityCompat;
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
-import com.example.alimama.MoodMapUI.SectionsPagerAdapter;
+import com.google.firebase.firestore.GeoPoint;
 
 import java.util.ArrayList;
 
-public class MoodMap extends AppCompatActivity implements MapViewFeedback{
+public class MoodMap extends AppCompatActivity implements MapViewFeedback, OnMapReadyCallback{
 
-    private static final int REQUEST_LOCATION_PERMISSION = 1;
-    private SectionsPagerAdapter sectionsPagerAdapter;
+    private GoogleMap mMap;
+    private Context context;
 
-    // tab event
+    private String currParticipant;
+    private Database db = new Database();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.context = this;
         setContentView(R.layout.activity_mood_map);
-        sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
+        Button myMoodBtn = findViewById(R.id.mood_map_my_map_btn);
+        Button friendMoodBtn = findViewById(R.id.mood_map_friend_map_btn);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mood_map_map_fragment);
+        currParticipant = getIntent().getStringExtra("USERNAME");
+        mapFragment.getMapAsync(this);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-       /* fab.setOnClickListener(new View.OnClickListener() {
+        friendMoodBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                db.retrieveAllLocatedMostRecentMoodEventsOfFriendsOfAParticipant(currParticipant, (MoodMap)context);
             }
-        }); */
+        });
+        myMoodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.retrieveAllLocatedMoodEventsOfAParticipant(currParticipant,(MoodMap)context);
+            }
+        });
     }
+
 
     @Override
     public void retrieveAllLocatedMoodEventsOfAParticipantSuccessfully(ArrayList<MoodEvent> moodEventsWithLocation) {
+        mMap.clear();
+        for(MoodEvent each: moodEventsWithLocation) {
+            GeoPoint location = each.getLocationOfMoodEvent();
+            mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),location.getLongitude())));
+
+        }
+
+
 
     }
 
@@ -60,8 +77,13 @@ public class MoodMap extends AppCompatActivity implements MapViewFeedback{
 
     @Override
     public void retrieveAllLocatedMostRecentMoodEventsOfFriendsOfAParticipantSuccessfully(ArrayList<MoodEvent> mostRecentMoodEventsOfFriendsOfAParticipantWithLocation) {
-        MyFriendMoodMap myFriendMoodMap = (MyFriendMoodMap) this.sectionsPagerAdapter.getItem(1);
-        myFriendMoodMap.setMapData(mostRecentMoodEventsOfFriendsOfAParticipantWithLocation);
+        mMap.clear();
+        for(MoodEvent each: mostRecentMoodEventsOfFriendsOfAParticipantWithLocation) {
+            GeoPoint location = each.getLocationOfMoodEvent();
+            mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),location.getLongitude())));
+
+        }
+
 
 
 
@@ -69,6 +91,39 @@ public class MoodMap extends AppCompatActivity implements MapViewFeedback{
 
     @Override
     public void failRetrieveAllLocatedMostRecentMoodEventsOfFriendsOfAParticipant(String message) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.setIndoorEnabled(true); // Indoor Map
+        googleMap.setMyLocationEnabled(false); // Location Sharing
+        UiSettings Ui = googleMap.getUiSettings();
+        Ui.setZoomControlsEnabled(true); //Zoom in zoom out event
+        Ui.setMapToolbarEnabled(false);
+        Ui.setMyLocationButtonEnabled(false);
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        mMap = googleMap;
+
+        LatLng UofA = new LatLng(53.526777, -113.527153); // Latitude, Longitude
+        mMap.addMarker(new MarkerOptions().position(UofA).title("Happy"));
+        CameraPosition UAlberta = CameraPosition.builder()
+                .target(UofA)
+                .zoom(15)
+                .bearing(0)
+                .tilt(45)
+                .build();
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(UAlberta), 3000, null);
+        mMap.addMarker(new MarkerOptions().position(new LatLng(53.528570, -113.524359)).title("Sad"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(53.527647, -113.524210)).title("Angry"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(53.526709, -113.521721)).title("Excited"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(53.526658, -113.524714)).title("Happy"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(53.526051, -113.526211)).title("Disappointment"));
+
+
 
     }
 }
