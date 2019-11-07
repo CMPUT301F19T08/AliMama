bd3a1ca342bd68a1729799a4aedcddc509b2c619
kingfxj <kingfu1012@gmail.com>
2019-11-07 06:22:39 -0700

    Update for test

---
+++ AliMama/app/src/androidTest/java/com/example/alimama/MoodEventClickListenerTest.java
@@ -0,0 +1,16 @@
+package com.example.alimama;
+
+import com.robotium.solo.Solo;
+import org.junit.Test;
+
+public class MoodEventClickListenerTest {
+    private Solo solo;
+
+    @Test
+    public void onEditClick() {
+    }
+
+    @Test
+    public void onDeleteClick() {
+    }
+}
