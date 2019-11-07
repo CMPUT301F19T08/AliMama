532a44a9fa85aff2127e49d2e83e7c9e9532836d
SkyZhangX <51038567+SkyZhangX@users.noreply.github.com>
2019-11-06 19:37:46 -0700

    delete all

--- AliMama/app/src/main/java/com/example/alimama/ClickDelegate.java
+++
@@ -1,5 +0,0 @@
-package com.example.alimama;
-
-public interface ClickDelegate {
-    void onFriendAddButtonClick(int position);
-}
--- AliMama/app/src/main/java/com/example/alimama/FriendPageClickDelegate.java
+++
@@ -1,5 +0,0 @@
-package com.example.alimama;
-
-public interface FriendPageClickDelegate {
-    void onFriendAddButtonClick(int position);
-}
