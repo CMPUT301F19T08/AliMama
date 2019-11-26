dbf3a577eb343d44ebc38a53930a4f94ccc21207
thuang2 <thuang2@ualberta.ca>
2019-11-25 17:48:07 -0700

    friend operation java doc finish

--- AliMama/app/src/main/java/com/example/alimama/friendOperation/contact/ContactPageAdapter.java
+++ AliMama/app/src/main/java/com/example/alimama/friendOperation/contact/ContactPageAdapter.java
@@ -1,4 +1,3 @@
-
 package com.example.alimama.friendOperation.contact;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,12 +13,20 @@ import java.util.ArrayList;
 
 import com.example.alimama.R;
 
+/**
+ * Manage the contact page adapter
+ * */
 public class ContactPageAdapter extends RecyclerView.Adapter<ContactPageAdapter.MyViewHolder> {
     private ArrayList<String> contactList;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
+    /**
+     * Provide a reference to the views for each data item
+     * Complex data items may need more than one view per item, and
+     * you provide access to all the views for a data item in a view holder
+     * */
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
         private ImageView pic;
@@ -32,30 +39,51 @@ public class ContactPageAdapter extends RecyclerView.Adapter<ContactPageAdapter.
             this.contactName = itemView.findViewById(R.id.contact_name);
         }
 
-
+        /**
+         * Get picture
+         * @return picture
+         * */
         public ImageView getPic() {
             return pic;
         }
 
+        /**
+         * Set picture
+         * */
         public void setPic(ImageView pic) {
             this.pic = pic;
         }
 
+        /**
+         * Get contact name
+         * @return contact name
+         * */
         public TextView getContactName() {
             return contactName;
         }
 
+        /**
+         * Set contact name
+         * */
         public void setContactName(TextView contactName) {
             this.contactName = contactName;
         }
     }
 
 
-
+    /**
+     * Set the contact page adapter
+     * @param contactDataList the array list of contact data
+     * */
     public ContactPageAdapter(ArrayList<String> contactDataList) {
         this.contactList = contactDataList;
     }
-    // Create new views (invoked by the layout manager)
+
+    /**
+     * Create new views (invoked by the layout manager)
+     * @param viewType the type of view
+     * @param parent the last view
+     */
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent,
                                            int viewType) {
@@ -66,7 +94,11 @@ public class ContactPageAdapter extends RecyclerView.Adapter<ContactPageAdapter.
         return viewHolder;
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
+    /**
+     * Replace the contents of a view
+     * @param position the position of click
+     * @param holder the holder of view
+     * */
     @Override
     public void onBindViewHolder(MyViewHolder holder,final int position) {
         // - get element from your dataset at this position
@@ -77,14 +109,13 @@ public class ContactPageAdapter extends RecyclerView.Adapter<ContactPageAdapter.
 
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
+    /**
+     * Get the size of your data set
+     * @return the size of contact list
+     * */
     @Override
     public int getItemCount() {
-
         return contactList.size();
     }
-
-
-
 
 }
